9e7807b693e774a035dc0c2bd92051e1866484de
Bernard Anderson <bernardanderson@gmail.com>
2017-02-13 23:09:36 -0600

    Completion of QM Code Challenge #1

---
+++ README.md
@@ -0,0 +1,20 @@
+# Parse The Preamble
+
+### Specs:
+> Create a Front-End Browser Rich Application that utilizes the following:
+
+### Technologies Used:
+> 1. HTML5, CSS, JavaScript
+> 2. [jQuery 3.1.1](https://jquery.com/): DOM Manipulation
+> 3. [Semantic-UI](http://semantic-ui.com/): DOM Styling
+
+### How to run:
+```
+1. Download or/clone this Repo
+2. Go to: `https://github.com/johnpapa/lite-server` and install "lite-server".  
+3. Navigate to the project folder in command line interface and type: `lite-server`  
+4. This will show at: `http://localhost:3000` in your local web browser.  
+```
+
+### Contributors:
+[Bernie Anderson](https://github.com/bernardanderson)  
---
+++ preamble.js
@@ -0,0 +1,39 @@
+// The preamble as taken from: https://constitutioncenter.org/interactive-constitution/preamble
+const usPreamble="We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.";
+
+// Initialize the word "counters"
+let letterCount = {
+    numberOfT: 0,
+    numberOfE: 0,
+    numberOfTE: 0
+};
+
+// Displays the Preamble on the DOM, so you aren't writing it in both the .html and .js files
+$('.preamble').text(usPreamble);
+
+// Removes the punctuation, gets rid of casing and splits the string into an array of the actual words
+let punctuationLessPreamble = usPreamble.replace(new RegExp(',|\\.', 'g'),"").toLowerCase().split(" ");
+
+// Cycles through the array, looks for first letter of 't', if found, checks to see if last letter is 'e'
+//  If neither are true, just checks the word for the last letter being 'e'   
+punctuationLessPreamble.forEach((element) => {
+ if (element[0] === "t") {
+     letterCount.numberOfT++;
+     if (element[element.length-1] === "e") {
+         letterCount.numberOfE++;
+         letterCount.numberOfTE++;
+     }
+ } else if (element[element.length-1] === "e") {
+    letterCount.numberOfE++;
+ }
+});
+
+// Displays the results the DOM
+$('.word-t').text(letterCount.numberOfT);
+$('.word-e').text(letterCount.numberOfE);
+$('.word-te').text(letterCount.numberOfTE);
+
+// In case you just want to look at the console instead of the DOM.
+console.log(`Words starting with 'T': ${letterCount.numberOfT}`);
+console.log(`Words ending with 'E': ${letterCount.numberOfE}`);
+console.log(`Words starting with 'T' & ending with 'E': ${letterCount.numberOfTE}`);
